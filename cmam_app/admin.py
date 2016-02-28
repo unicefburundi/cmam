39ed3847bd500f0160e50116b2a206d352b5ccb7
mbanje <mbanjejvernack@gmail.com>
2016-02-28 12:15:50 +0200

    Fix bug about migrations and edit admin.py file

--- cmam_app/admin.py
+++ cmam_app/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from cmam_app.models import *
 
 # Register your models here.
+
+
+admin.site.register(Product)
+admin.site.register(Facility)
+admin.site.register(Stock)
+admin.site.register(Reporter)
+admin.site.register(User)
+admin.site.register(Report)
+admin.site.register(Sortie)
+admin.site.register(Reception)
+admin.site.register(StockOutReport)
+admin.site.register(ProductsReceptionReport)
+admin.site.register(ProductsTranferReport)
+admin.site.register(IncomingPatientsReport)
+admin.site.register(OutgoingPatientsReport)
+admin.site.register(StockReport)
+admin.site.register(ProductStockReport)
