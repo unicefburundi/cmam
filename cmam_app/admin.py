cf64b2b677daa63fbcb3dff78dff17b4d9dc6c78
RUGANO Allan Stockman <kuristockyrugano@gmail.com>
2016-04-25 15:11:31 +0200

    adding import-export

--- cmam_app/admin.py
+++ cmam_app/admin.py
@@ -1,13 +1,57 @@
 from django.contrib import admin
 from cmam_app.models import *
+from import_export import resources
+from import_export.admin import ExportMixin
 
-# Register your models here.
+class ProductAdminResource(resources.ModelResource):
+    class Meta:
+        model =Product
+        fields = ('designation', 'general_measuring_unit', 'dose_par_semaine', 'quantite_en_stock_central')
+
+class ProductAdmin(ExportMixin, admin.ModelAdmin):
+    resource_class = ProductAdminResource
+    list_display = ('designation', 'general_measuring_unit', 'dose_par_semaine', 'quantite_en_stock_central')
+    search_fields = ('designation', 'general_measuring_unit' )
+    list_filter = ( 'dose_par_semaine',)
 
 
-admin.site.register(Product)
-admin.site.register(Facility)
-admin.site.register(Stock)
-admin.site.register(Reporter)
+class FacilityAdminResource(resources.ModelResource):
+    class Meta:
+        model =Facility
+        fields = ('id_facility', 'name', 'facility_level__name')
+
+class FacilityAdmin(ExportMixin, admin.ModelAdmin):
+    resource_class = FacilityAdminResource
+    list_display = ('id_facility', 'name', 'facility_level')
+    search_fields = ('id_facility', 'name')
+    list_filter = ( 'facility_level',)
+
+class StockAdminResource(resources.ModelResource):
+    class Meta:
+        model =Stock
+        fields = ('id_facility', 'product', 'quantity')
+
+class StockAdmin(ExportMixin, admin.ModelAdmin):
+    resource_class = StockAdminResource
+    list_display = ('id_facility', 'product', 'quantity')
+    search_fields = ('id_facility', 'product')
+    list_filter = ( 'product',)
+
+class ReporterAdminResource(resources.ModelResource):
+    class Meta:
+        model = Reporter
+        fields = ('facility', 'phone_number', 'supervisor_phone_number')
+
+class ReporterAdmin(ExportMixin, admin.ModelAdmin):
+    resource_class = ReporterAdminResource
+    list_display = ('facility', 'phone_number', 'supervisor_phone_number')
+    search_fields = ('facility', 'phone_number', 'supervisor_phone_number')
+    list_filter = ( 'facility__facility_level__name',)
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Facility, FacilityAdmin)
+admin.site.register(Stock, StockAdmin)
+admin.site.register(Reporter, ReporterAdmin)
 admin.site.register(User)
 admin.site.register(Report)
 admin.site.register(Sortie)
