05aa937b8e1393eb7329f0018a297acbf48fe461
RUGANO Allan Stockman <kuristockyrugano@gmail.com>
2016-10-05 12:49:29 +0200

    updated the districts level for programs

--- staticfiles/js/cmam-programs.js
+++ staticfiles/js/cmam-programs.js
@@ -5,8 +5,8 @@ app.controller('pgrmCtrl', ['$scope', '$http', function($scope, $http) {
     $http.get("/cmam/inoutreport/?report__facility__facility_level__name=CDS")
     .then(function (response) {
         var weeks = {},
-            columns = {},
-            sumCols = ['total_debut_semaine','ptb', 'oedemes', 'rechute', 'readmission', 'transfert_interne_i','gueri', 'deces', 'abandon', 'non_repondant', 'transfert_interne_o'], data = response.data;
+        columns = {},
+        sumCols = ['total_debut_semaine','ptb', 'oedemes', 'rechute', 'readmission', 'transfert_interne_i','gueri', 'deces', 'abandon', 'non_repondant', 'transfert_interne_o'], data = response.data;
 
         $.each(data, function(index, obj) {
             if (!weeks[obj['week']]) {
@@ -29,13 +29,13 @@ app.controller('pgrmCtrl', ['$scope', '$http', function($scope, $http) {
         $scope.lescds =  weeks;
         $scope.cdsgnrl = columns;
         $scope.sommecds = columns;
-        });
+    });
 
     $http.get("/cmam/inoutreport/?report__facility__facility_level__name=Hospital")
     .then(function (response) {
         var weeks = {},
-            columns = {},
-            sumCols = ['total_debut_semaine','ptb', 'oedemes', 'rechute', 'readmission', 'transfert_interne_i','gueri', 'deces', 'abandon', 'non_repondant', 'transfert_interne_o'], data = response.data;
+        columns = {},
+        sumCols = ['total_debut_semaine','ptb', 'oedemes', 'rechute', 'readmission', 'transfert_interne_i','gueri', 'deces', 'abandon', 'non_repondant', 'transfert_interne_o'], data = response.data;
 
         $.each(data, function(index, obj) {
             if (!weeks[obj['week']]) {
@@ -59,13 +59,13 @@ app.controller('pgrmCtrl', ['$scope', '$http', function($scope, $http) {
         $scope.leshopitaux =  weeks;
         $scope.hopitauxgnrl =  columns;
         $scope.sommehopitaux = columns;
-        });
+    });
 
         // province
         $http.get("/bdiadmin/province/")
-          .then(function (response) {
-                $scope.provinces = response.data;
-          });
+        .then(function (response) {
+            $scope.provinces = response.data;
+        });
 
         $scope.update_province = function () {
             var province = $scope.province;
@@ -73,17 +73,17 @@ app.controller('pgrmCtrl', ['$scope', '$http', function($scope, $http) {
 
                 //update districts
                 $http.get("/cmam/provinces/" + province.code + "/" )
-                    .then(function (response) {
-                        $scope.districts = response.data.districts;
-                  });
+                .then(function (response) {
+                    $scope.districts = response.data.districts;
+                });
 
                 // update hopitaux
                 $http.get("/cmam/inoutreport/?search=" + province.code+ "&report__facility__facility_level__name=Hospital")
-                    .then(function (response) {
-                        var weeks = {},
-                            columns = {},
-                            sumCols = ['total_debut_semaine','ptb', 'oedemes', 'rechute', 'readmission', 'transfert_interne_i','gueri', 'deces', 'abandon', 'non_repondant', 'transfert_interne_o'],
-                            data = response.data;
+                .then(function (response) {
+                    var weeks = {},
+                    columns = {},
+                    sumCols = ['total_debut_semaine','ptb', 'oedemes', 'rechute', 'readmission', 'transfert_interne_i','gueri', 'deces', 'abandon', 'non_repondant', 'transfert_interne_o'],
+                    data = response.data;
 
                     $.each(data, function(index, obj) {
                         if (!weeks[obj['week']]) {
@@ -107,49 +107,111 @@ app.controller('pgrmCtrl', ['$scope', '$http', function($scope, $http) {
                     $scope.leshopitaux =  weeks;
                     $scope.hopitauxprov =  columns;
                     $scope.sommehopitaux = columns;
-              });
+                });
                 // update cds
                 $http.get("/cmam/inoutreport/?search=" + province.code+ "&report__facility__facility_level__name=CDS")
-                    .then(function (response) {
-                        var weeks = {},
-                            columns = {},
-                            sumCols = ['total_debut_semaine','ptb', 'oedemes', 'rechute', 'readmission', 'transfert_interne_i','gueri', 'deces', 'abandon', 'non_repondant', 'transfert_interne_o'],
-                            data = response.data;
+                .then(function (response) {
+                    var weeks = {},
+                    columns = {},
+                    sumCols = ['total_debut_semaine','ptb', 'oedemes', 'rechute', 'readmission', 'transfert_interne_i','gueri', 'deces', 'abandon', 'non_repondant', 'transfert_interne_o'],
+                    data = response.data;
 
-                        $.each(data, function(index, obj) {
-                            if (!weeks[obj['week']]) {
-                                weeks[obj['week']] = {};
+                    $.each(data, function(index, obj) {
+                        if (!weeks[obj['week']]) {
+                            weeks[obj['week']] = {};
+                        }
+                        $.each(sumCols, function (index, col) {
+                            if (!weeks[obj['week']][col]) {
+                                weeks[obj['week']][col] = 0;
                             }
-                            $.each(sumCols, function (index, col) {
-                                if (!weeks[obj['week']][col]) {
-                                    weeks[obj['week']][col] = 0;
-                                }
-                                if (!columns[col]) {
-                                    columns[col] = 0;
-                                }
-                                var val = parseFloat(obj[col]);
-                                if (!isNaN(val)) {
-                                    weeks[obj['week']][col] += val;
-                                    columns[col] += val;
-                                }
-                            });
+                            if (!columns[col]) {
+                                columns[col] = 0;
+                            }
+                            var val = parseFloat(obj[col]);
+                            if (!isNaN(val)) {
+                                weeks[obj['week']][col] += val;
+                                columns[col] += val;
+                            }
                         });
-                        $scope.lescds =  weeks;
-                        $scope.cdsprov =  columns;
-                        $scope.sommecds = columns;
-                        });
+                    });
+                    $scope.lescds =  weeks;
+                    $scope.cdsprov =  columns;
+                    $scope.sommecds = columns;
+                });
             }
         };
 
         // district
         $scope.update_district = function () {
             var district = $scope.district;
-            if ($scope.province) {
+            if ($scope.district) {
               $http.get("/cmam/districts/" + district.code + "/" )
-                .then(function (response) {
+              .then(function (response) {
                   $scope.cds = response.data.cds;
               });
-            }
-        };
-}]);
+
+              // update cds
+              $http.get("/cmam/inoutreport/?search=" + district.code+ "&report__facility__facility_level__name=CDS")
+                .then(function (response) {
+                    var weeks = {},
+                    columns = {},
+                    sumCols = ['total_debut_semaine','ptb', 'oedemes', 'rechute', 'readmission', 'transfert_interne_i','gueri', 'deces', 'abandon', 'non_repondant', 'transfert_interne_o'],
+                    data = response.data;
+
+                    $.each(data, function(index, obj) {
+                        if (!weeks[obj['week']]) {
+                            weeks[obj['week']] = {};
+                        }
+                        $.each(sumCols, function (index, col) {
+                            if (!weeks[obj['week']][col]) {
+                                weeks[obj['week']][col] = 0;
+                            }
+                            if (!columns[col]) {
+                                columns[col] = 0;
+                            }
+                            var val = parseFloat(obj[col]);
+                            if (!isNaN(val)) {
+                                weeks[obj['week']][col] += val;
+                                columns[col] += val;
+                            }
+                        });
+                    });
+                    $scope.lescds =  weeks;
+                    $scope.cdsdistr =  columns;
+                    $scope.sommecds = columns;
+                });
+              // update hopitaux
+              $http.get("/cmam/inoutreport/?search=" + district.code+ "&report__facility__facility_level__name=Hospital")
+                .then(function (response) {
+                    var weeks = {},
+                    columns = {},
+                    sumCols = ['total_debut_semaine','ptb', 'oedemes', 'rechute', 'readmission', 'transfert_interne_i','gueri', 'deces', 'abandon', 'non_repondant', 'transfert_interne_o'],
+                    data = response.data;
+
+                    $.each(data, function(index, obj) {
+                        if (!weeks[obj['week']]) {
+                            weeks[obj['week']] = {};
+                        }
+                        $.each(sumCols, function (index, col) {
+                            if (!weeks[obj['week']][col]) {
+                                weeks[obj['week']][col] = 0;
+                            }
+                            if (!columns[col]) {
+                                columns[col] = 0;
+                            }
+                            var val = parseFloat(obj[col]);
+                            if (!isNaN(val)) {
+                                weeks[obj['week']][col] += val;
+                                columns[col] += val;
+                            }
+                        });
+                    });
+
+                    $scope.leshopitaux =  weeks;
+                    $scope.hopitauxdistr =  columns;
+                    $scope.sommehopitaux = columns;
+                });
+          }
+      };
+  }]);
 
