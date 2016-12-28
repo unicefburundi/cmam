2dcc1084a2c7827570ad608bbf8a4c105de8c107
RUGANO Allan Stockman <kuristockyrugano@gmail.com>
2016-12-28 10:27:51 +0200

    better admin

--- bdiadmin/admin.py
+++ bdiadmin/admin.py
@@ -3,10 +3,12 @@ from import_export import resources
 from import_export.admin import ExportMixin
 from bdiadmin.models import *
 
+
 class ProvinceResource(resources.ModelResource):
     class Meta:
         model = Province
         fields = ('name', 'code')
+
 
 class ProvinceAdmin(ExportMixin, admin.ModelAdmin):
     resource_class = ProvinceResource
@@ -18,6 +20,7 @@ class CommuneResource(resources.ModelResource):
     class Meta:
         model = Commune
         fields = ('name', 'code', 'province__name')
+
 
 class CommuneAdmin(ExportMixin, admin.ModelAdmin):
     resource_class = CommuneResource
@@ -31,6 +34,7 @@ class CollineResource(resources.ModelResource):
         model = Colline
         fields = ('name', 'code', 'commune__name', 'commune__province__name')
 
+
 class CollineAdmin(ExportMixin, admin.ModelAdmin):
     resource_class = CollineResource
     search_fields = ('name', 'code')
@@ -40,10 +44,12 @@ class CollineAdmin(ExportMixin, admin.ModelAdmin):
     def province(self, obj):
         return obj.commune.province.name
 
+
 class ProfileUserResource(resources.ModelResource):
     class Meta:
         model = ProfileUser
         fields = ('user__email', 'telephone', 'level', 'user__username')
+
 
 class ProfileUserAdmin(ExportMixin, admin.ModelAdmin):
     resource_class = ProfileUserResource
@@ -56,25 +62,31 @@ class ProfileUserAdmin(ExportMixin, admin.ModelAdmin):
     def email(self, obj):
         return obj.user.email
 
+
 class DistrictResource(resources.ModelResource):
     class Meta:
         model = District
         fields = ('name', 'code', 'province')
 
+
 class DistrictAdmin(ExportMixin, admin.ModelAdmin):
     resource_class = DistrictResource
     search_fields = ('name', 'code')
     list_display = ('name', 'code', 'province')
+    list_filter = ('province',)
+
 
 class CDSResource(resources.ModelResource):
     class Meta:
         model = CDS
         fields = ('name', 'code', 'district', 'district__province__name')
 
+
 class CDSAdmin(ExportMixin, admin.ModelAdmin):
     resource_class = CDSResource
     search_fields = ('name', 'code')
     list_display = ('name', 'code', 'district', 'province')
+    list_filter = ('district__province', 'district', )
 
     def province(self, obj):
         return obj.district.province
